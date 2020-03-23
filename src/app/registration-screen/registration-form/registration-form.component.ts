79ce3da9efd40bf0b3e8ac960f98892810d6f274
Júlio Oliveira <juliocesar_morais@outlook.com>
2020-03-22 22:43:23 -0300

    Centralizar o RegistrationFormComponent

--- src/app/registration-screen/registration-form/registration-form.component.ts
+++ src/app/registration-screen/registration-form/registration-form.component.ts
@@ -9,7 +9,7 @@ interface Sex {
 @Component({
   selector: 'app-registration-form',
   templateUrl: './registration-form.component.html',
-  styleUrls: ['./registration-form.component.scss']
+  styleUrls: ['./registration-form.component.css']
 })
 export class RegistrationFormComponent implements OnInit {
 
